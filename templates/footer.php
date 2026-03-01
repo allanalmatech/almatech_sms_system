2dfb74a40c12531683b63bd6bdb6da5f7e90e9d9
almatechuganda <68955030+almatechuganda@users.noreply.github.com>
2026-03-01 18:21:44 +0300

    Added tawk script

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,19 @@
 <?php
 declare(strict_types=1);
 ?>
+ <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5d08d28453d10a56bd7aa2d5/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <footer class="border-top py-3 mt-4 bg-white">
   <div class="container-fluid d-flex justify-content-between flex-wrap gap-2 text-muted small">
     <span>AlmaTech SMS Platform</span>
